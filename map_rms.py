cd77feb42bbdcd310cc607031ab4be555d6df43b
Jaime Pineda <jaime.e.pineda@gmail.com>
2016-07-22 13:46:11 +0200

    Figures for other regions finished (#1)
    - New figure with all histograms for same region, but different colors... colors might be tricky to choose properly
    - looks like a good starting point
    - all figures are done using the DR1_rebase3 files
    - Main remaining point: I need to check if the edges have been trimmed or not, if not, then we need to remove the map edges, and that would remove the ugly tail in the histograms

--- map_rms.py
+++ map_rms.py
@@ -7,7 +7,7 @@ import os
 from astropy.io import fits
 import aplpy
 
-region_list=['L1688', 'B18']
+region_list=['L1688', 'B18', 'NGC1333', 'OrionA']
 line_list=['NH3_11','NH3_22','NH3_33','C2S','HC5N','HC7N_21_20','HC7N_22_21']
 label_list=['NH$_3$(1,1)','NH$_3$(2,2)','NH$_3$(3,3)','C$_2$S','HC$_5$N',
             'HC$_7$N (21-20)','HC$_7$N (22-21)']
@@ -16,6 +16,11 @@ color_table='Blues'
 text_color='black'
 beam_color='#d95f02'  # previously used '#E31A1C'
 color_hist='#a6bddb'
+
+color_hall= ['#a6cee3', '#fdbf6f', '#33a02c', '#b2df8a', '#fb9a99', '#e31a1c', 
+             '#1f78b4']
+# color_hall= ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02',
+#              '#a6761d']
 
 bin_size=0.01 # K
 
@@ -30,16 +35,34 @@ def setup_plot_parameters(region='L1688'):
     the array loops over the lines in hte order of line_list
     """
     if region=='L1688':
-        param={'size_x' : 9, 'size_y' : 7, 'scale_bar' : 0.1*u.pc, 
+        param={'size_x' : 9, 'size_y' : 7, 
+               'scalebar_size' : 0.1*u.pc, 'scalebar_pos' : 'bottom right',
                'distance' : 145.*u.pc, 'beam_pos' : 'bottom left',
                'label_xpos' : 0.025, 'label_ypos' : 0.1,
                'label_align' : 'left',
                'rms_min' : np.array([0.05, 0.05, 0.05, 0.1, 0.05, 0.05, 0.05]),
                'rms_max' : np.array([0.75, 0.75, 0.75, 1.0, 0.75, 0.75, 0.75]) }
     elif region=='B18':
-        param={'size_x' : 19, 'size_y' : 7, 'scale_bar' : 0.1*u.pc, 
+        param={'size_x' : 19, 'size_y' : 7, 
+               'scalebar_size' : 0.1*u.pc, 'scalebar_pos' : 'bottom right',
                'distance' : 145.*u.pc, 'beam_pos' : 'top left',
                'label_xpos' : 0.025, 'label_ypos' : 0.9,
+               'label_align' : 'left',
+               'rms_min' : np.array([0.05, 0.05, 0.05, 0.1, 0.05, 0.05, 0.05]),
+               'rms_max' : np.array([0.75, 0.75, 0.75, 1.0, 0.75, 0.75, 0.75]) }
+    elif region=='NGC1333':
+        param={'size_x' : 8, 'size_y' : 12, 
+               'scalebar_size' : 0.1*u.pc, 'scalebar_pos' : 'top right',
+               'distance' : 250.*u.pc, 'beam_pos' : 'bottom left',
+               'label_xpos' : 0.025, 'label_ypos' : 0.075,
+               'label_align' : 'left',
+               'rms_min' : np.array([0.05, 0.05, 0.05, 0.1, 0.05, 0.05, 0.05]),
+               'rms_max' : np.array([0.75, 0.75, 0.75, 1.0, 0.75, 0.75, 0.75]) }
+    elif region=='OrionA':
+        param={'size_x' : 4.5, 'size_y' : 11, 
+               'scalebar_size' : 0.1*u.pc, 'scalebar_pos' : 'bottom right',
+               'distance' : 450.*u.pc, 'beam_pos' : 'top left',
+               'label_xpos' : 0.025, 'label_ypos' : 0.925,
                'label_align' : 'left',
                'rms_min' : np.array([0.05, 0.05, 0.05, 0.1, 0.05, 0.05, 0.05]),
                'rms_max' : np.array([0.75, 0.75, 0.75, 1.0, 0.75, 0.75, 0.75]) }
@@ -50,6 +73,11 @@ def setup_plot_parameters(region='L1688'):
 
 for region_i in region_list:
     plot_param=setup_plot_parameters(region=region_i)
+    fig_hall=plt.figure(figsize=(6,6))
+    ax_hall = fig_hall.add_subplot(111)
+    v_hall_max=np.max(plot_param['rms_max'])
+    v_hall_min=np.min(plot_param['rms_min'])
+    nbin_hall=int(np.ceil( np.abs(v_hall_max-v_hall_min)/ bin_size))
     for i in range(len(line_list)):
         line_i=line_list[i]
         label_i=label_list[i]
@@ -71,10 +99,11 @@ for region_i in region_list:
             # Scale bar
             # magic line of code to obtain scale in arcsec obtained from 
             # http://www.astropy.org/astropy-tutorials/Quantities.html
-            ang_sep =  (plot_param['scale_bar'].to(u.au)/plot_param['distance']).to(u.arcsec, equivalencies=u.dimensionless_angles())
+            ang_sep =  (plot_param['scalebar_size'].to(u.au)/plot_param['distance']).to(u.arcsec, equivalencies=u.dimensionless_angles())
             fig.add_scalebar(ang_sep.to(u.degree))
+            fig.scalebar.set_corner(plot_param['scalebar_pos'])
             fig.scalebar.set(color=text_color)
-            fig.scalebar.set_label('{0:4.2f}'.format(plot_param['scale_bar']))
+            fig.scalebar.set_label('{0:4.2f}'.format(plot_param['scalebar_size']))
             # Labels
             fig.add_label(plot_param['label_xpos'], plot_param['label_ypos'], 
                           '{0}\n{1}'.format(region_i,label_i), 
@@ -99,14 +128,27 @@ for region_i in region_list:
             n, bins, patches = ax.hist( myarray, weights=weights, bins=nbin, 
                                        facecolor=color_hist, range=(v_min,v_max), 
                                        histtype='stepfilled') 
-            plt.xlabel('rms (K)')
-            plt.ylabel('Fraction of Pixels')
+            ax.set_xlabel('rms (K)')
+            ax.set_ylabel('Fraction of Pixels')
             ax.text(0.9, 0.9, '{0}\n{1}'.format(region_i,label_i),
                     horizontalalignment='right',
                     verticalalignment='top',
                     transform=ax.transAxes)
-            plt.xlim(xmin=v_min,xmax=v_max)
+            ax.set_xlim(xmin=v_min,xmax=v_max)
             fig.savefig('figures/{0}_{1}_{2}_rms_hist.pdf'.format(region_i,line_i,extension),adjust_bbox=True)
+            # now do the histogram of all lines together
+            n, bins, patches = ax_hall.hist( myarray, weights=weights, bins=nbin_hall, 
+                                       range=(v_hall_min,v_hall_max), color=color_hall[i], 
+                                       histtype='step', label=label_i) 
         else:
             print('File {0} not found'.format(file_rms))
+    # after for loop is finished then close things
+    ax_hall.set_xlabel('rms (K)')
+    ax_hall.set_ylabel('Fraction of Pixels')
+    ax_hall.set_xlim(xmin=v_hall_min,xmax=v_hall_max)
+    leg=ax_hall.legend(frameon=False, loc=1, title=region_i)
+    leg_text=leg.get_texts()
+    for index in range(len(leg_text)):
+        plt.setp( leg_text[index], color=color_hall[index] )
+    fig_hall.savefig('figures/{0}_all_{1}_rms_hist.pdf'.format(region_i,extension),adjust_bbox=True)
 
