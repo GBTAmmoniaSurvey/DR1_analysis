1ec69a394bd4f98fa9fab2950e23f2d164f526f0
rfriesen <friesen.rae@gmail.com>
2016-09-05 23:32:08 -0400

    Using binary_opening

--- property_maps.py
+++ property_maps.py
@@ -3,19 +3,27 @@ import astropy.units as u
 import warnings
 import numpy as np
 import os
+from scipy.ndimage import binary_opening
 
 from astropy.io import fits
 import aplpy
 
 from config import plottingDictionary
 
+def mask_binary(imageHDU,LowestContour,selem):
+    map = imageHDU[0].data
+    mask = binary_opening(map > LowestContour, selem)
+    MaskedMap = mask*map
+    imageHDU[0].data = MaskedMap
+    return imageHDU, mask
+
 """
 Make property map plots for DR1 regions 
 Addresses issue #3 in DR1_analysis
 """
 
-#region_list=['L1688', 'B18', 'NGC1333', 'OrionA']
-region_list = ['OrionA']
+region_list=['L1688', 'B18', 'NGC1333', 'OrionA']
+#region_list = ['NGC1333']
 extension='DR1_rebase3'
 # Property maps to plot
 par_list = ['Vlsr','Sigma','Tkin','Tex','N_NH3']
@@ -31,11 +39,18 @@ beam_face = 'None'
 beam_edge = 'black'
 nan_color = '0.95'
 #beam_color='#d95f02'  # previously used '#E31A1C'
+# Masking of small (noisy) regions
+selem = np.array([[0,1,0],[1,1,1],[0,1,0]])
+maskLim = [0.25,0.5,0.5,0.2]
 
-for region in region_list:
+for region_i in range(len(region_list)):
+    region = region_list[region_i]
     plot_param=plottingDictionary[region]
     file_w11='{0}/{0}_NH3_11_{1}_mom0_QA_trim.fits'.format(region,extension)
     cont_levs=2**np.arange( 0,10)*plot_param['w11_step']
+    LowestContour = cont_levs[0]*maskLim[region_i]
+    w11_hdu = fits.open(file_w11)
+    w11_hdu, mask = mask_binary(w11_hdu,LowestContour,selem)
     pmin_list = plot_param['pmin_list']
     pmax_list = plot_param['pmax_list']
     p_step    = plot_param['p_step']
@@ -43,15 +58,21 @@ for region in region_list:
         par = par_list[par_i]
         label_i = label_list[par_i]
         par_file = '{0}/parameterMaps/{0}_{1}_{2}_flag.fits'.format(region,par,extension)
-        par_hdu = fits.open(par_file)
-        v_min = np.max([pmin_list[par_i],np.nanmin(par_hdu[0].data)])
-        v_max = np.min([pmax_list[par_i],np.nanmax(par_hdu[0].data)])
         if os.path.isfile(par_file):
-            fig=aplpy.FITSFigure(par_file,figsize=(plot_param['size_x'], plot_param['size_y']))
+            par_hdu = fits.open(par_file)
+            par_data = par_hdu[0].data
+            MaskedPar = mask * par_data
+            mask2 = binary_opening(MaskedPar, selem)
+            MaskedPar = mask2 * MaskedPar
+            MaskedPar[MaskedPar == 0] = np.nan 
+            par_hdu[0].data = MaskedPar
+            v_min = np.max([pmin_list[par_i],np.nanmin(MaskedPar)])
+            v_max = np.min([pmax_list[par_i],np.nanmax(MaskedPar)])
+            fig=aplpy.FITSFigure(par_hdu,figsize=(plot_param['size_x'], plot_param['size_y']))
             fig.show_colorscale(cmap=ctable_list[par_i],vmin=v_min,vmax=v_max)
             fig.set_nan_color(nan_color)
             # Moment contours
-            fig.show_contour(file_w11,colors=cont_color,levels=cont_levs,
+            fig.show_contour(w11_hdu,colors=cont_color,levels=cont_levs,
                              linewidths=cont_lw)
             # Ticks
             fig.ticks.set_color(text_color)
@@ -76,15 +97,16 @@ for region in region_list:
                           '{0}\n{1}'.format(region,label_i), 
                           relative=True, color=text_color, 
                           horizontalalignment=plot_param['label_align'])
-            # add colorbar. Colorbar labels not always ideal. 
+            # Add colorbar with defined tick labels
             colorbar_step = p_step[par_i]
             # make nice tick labels using defined colorbar step
             ticks = np.arange(np.ceil((v_max-v_min)/colorbar_step)+2)*colorbar_step + np.int(v_min)
+            #fig.add_colorbar(location='top',width=0.15,pad=0,ticks=ticks)
+            #fig.colorbar.set_width(0.15)
             fig.add_colorbar()
-            fig.colorbar.set_width(0.15)
             fig.colorbar.show(box_orientation='horizontal', width=0.1, pad=0.0, location='top',ticks=ticks)
             # fig.set_system_latex(True)
-            fig.save( 'figures/{0}_{1}_{2}_map.pdf'.format(region,extension,par),adjust_bbox=True)#, bbox_inches='tight')
+            fig.save( 'figures/{0}_{1}_{2}_map.pdf'.format(region,extension,par),adjust_bbox=True,dpi=100)#, bbox_inches='tight')
             fig.close()
         else:
             print('File {0} not found'.format(par_file))
