aaccc9a9b1678b1b81b196947831845d197e0960
rfriesen <friesen.rae@gmail.com>
2016-11-28 12:54:39 -0500

    Added annotations to some plots

--- map_mom0.py
+++ map_mom0.py
@@ -18,7 +18,7 @@ label_list=['NH$_3$(1,1)','NH$_3$(2,2)','NH$_3$(3,3)','C$_2$S','HC$_5$N',
 extension='DR1_rebase3'
 color_table='magma'
 text_color='black'
-text_size = 14
+fix_text_size = 14
 b18_text_size = 20
 beam_color='#d95f02'  # previously used '#E31A1C'
 # Masking of small (noisy) regions
@@ -34,8 +34,10 @@ for region_i in region_list:
     mask = binary_opening(map > LowestContour, selem)
     MaskedMap = mask*map
     w11_hdu[0].data = MaskedMap
-    if region_i == 'B18':
+    if region_i in ['B18','NGC1333']:
         text_size = b18_text_size
+    else:
+        text_size = fix_text_size
     for i in range(len(line_list)):
         line_i=line_list[i]
         label_i=label_list[i]
@@ -47,6 +49,14 @@ for region_i in region_list:
             if line_i == 'NH3_11':
                 fig.show_colorscale( cmap=color_table,vmin=v_min, vmax=v_max, stretch=plot_param['mom0_stretch'],vmid=v_min-(1.*np.abs(v_min)))
                 cbar_ticks = [0,3,6,12,24,48,96]
+                # add colorbar
+                fig.add_colorbar()
+                #fig.colorbar.set_width(0.15)
+                fig.colorbar.show( box_orientation='horizontal', width=0.1, pad=0.0, ticks=cbar_ticks,
+                                   location='top', axis_label_text='Integrated Intensity (K km s$^{-1}$)')
+            elif (line_i in ['NH3_22','NH3_33']) & (region_i == 'OrionA'): 
+                fig.show_colorscale( cmap=color_table,vmin=v_min, vmax=v_max, stretch=plot_param['mom0_stretch'],vmid=v_min-(1.*np.abs(v_min)))
+                cbar_ticks = [0,1,2,3,6,12]
                 # add colorbar
                 fig.add_colorbar()
                 #fig.colorbar.set_width(0.15)
@@ -85,6 +95,22 @@ for region_i in region_list:
             fig.scalebar.set_font(family='sans_serif',size=text_size)
             fig.scalebar.set(color=text_color)
             fig.scalebar.set_label('{0:4.2f}'.format(plot_param['scalebar_size']))
+            # Include some objects if identified in text
+            if (line_i == 'NH3_33') & (region_i == 'NGC1333'):
+                fig.show_markers(52.265417,31.26444,marker='^',s=50,edgecolor='white',
+                                 facecolor='None',zorder=4) # SVS 13
+                fig.show_markers(52.245833,31.33333,marker='v',s=50,edgecolor='white',
+                                 facecolor='None',zorder=4) # HH 12
+            if (line_i == 'NH3_33') & (region_i == 'L1688'):
+                fig.show_arrows(246.4375,-24.46572,-0.03,0,color='white',width=0.4,head_width=3,head_length=3,zorder=4) # HD 147889
+                fig.add_label(0.9,0.53,'HD 147889',relative=True,color='white',family='sans_serif',
+                              size=text_size*0.9)
+            if (line_i == 'NH3_33') & (region_i == 'OrionA'):
+                fig.add_label(0.45,0.6,'Orion Bar',relative=True,color='white',family='sans_serif',
+                              size=text_size*0.9)
+                fig.show_arrows(83.9167,-5.333,-0.08,-0.04,color='white',width=0.8,zorder=4)
+                fig.add_label(0.42,0.69,'Orion BN/KL',relative=True,color='white',family='sans_serif',
+                              size=text_size*0.9)
             # Labels
             fig.add_label(plot_param['label_xpos'], plot_param['label_ypos'], 
                           '{0}\n{1}'.format(region_i,label_i), 
